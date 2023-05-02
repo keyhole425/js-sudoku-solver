06a8239c261f23f3483fd98f6955a69573af2894
kylecombeer <kyle-combeer@users.noreply.github.com>
2023-05-02 11:41:28 +0930

    Add Reset button that will put the grid back to the initial state. Modified favicon to look a little nicer and more relevant.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import {calculateSolution} from './utils/solver';
 import './App.css';
 
+// Testing State
 const START_GRID = [
   [4, 0, 5, 7, 0, 0, 0, 0, 6],
   [0, 0, 0, 0, 0, 2, 0, 9, 0],
@@ -11,21 +12,38 @@ const START_GRID = [
   [0, 0, 0, 0, 0, 3, 8, 4, 0],
   [3, 0, 9, 0, 2, 6, 0, 0, 0],
   [0, 5, 0, 0, 0, 0, 0, 0, 4],
-  [0, 0, 0, 9, 5, 0, 0, 6, 0],
+  [0, 0, 0, 9, 5, 0, 0, 6, 0]
+];
+
+// Start State
+const EMPTY_GRID = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
 function SudokuSolver() {
-  const [grid, setGrid] = useState(START_GRID);
+  const [grid, setGrid] = useState(JSON.parse(JSON.stringify(START_GRID)));
 
   const handleInputChange = (index, row, e) => {
-    const newGrid = [...grid];
-    newGrid[row][index] = parseInt(e.target.value);
-    setGrid(newGrid);
+    grid[row][index] = parseInt(e.target.value);
+    setGrid(grid);
   };
 
   const solveSudoku = async () => {
     const newGrid = await calculateSolution(grid);
     setGrid(newGrid);
+  }
+
+  const resetSudoku = () => {
+    console.log('reset to start grid:', START_GRID);
+    setGrid(JSON.parse(JSON.stringify(START_GRID)));
   }
 
   const generateSudokuRow = (row, rowIndex) => {
@@ -77,6 +95,7 @@ function SudokuSolver() {
           Once you are happy, click the "Solve" button below to see the solution.
         </p>
         <button onClick={solveSudoku} className='solve-button'>Solve</button>
+        <button onClick={resetSudoku} className='solve-button'>Reset</button>
       </div>
     </div>
   );
